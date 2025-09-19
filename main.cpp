77b0146e3228fb478cb5a02222812e7c68526cf4
Jasmine <jazzyysuarez@gmail.com>
2025-09-18 23:32:23 -0700

    update

--- main.cpp
+++ main.cpp
@@ -7,11 +7,28 @@ using namespace std;
 struct Customer {
     string name;
     int tableNum;
+    int numOrders;
     string * orders;
+
+    ~Customer() {
+        if (orders)
+            delete [] orders;
+        orders = nullptr;
+    }
 };
+
+void inputCustomer(Customer *);
 
 int main() {
 
-
     return 0;
+}
+
+void inputCustomer(Customer * cptr) {
+    cout << "Enter customer name: ";
+    getline(cin, cptr->name);
+    cout << "Enter table number: ";
+    cin >> cptr->tableNum;
+    cout << "How many orders? ";
+    cin >> cptr->numOrders;
 }
